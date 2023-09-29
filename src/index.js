c604a9f8a2d57e39c87fdebba1550a8170aaf789
Sagar <you@example.com>
2023-09-29 14:52:59 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <div>
+        <StrictMode>
+        <App></App>
+      
+        </StrictMode>
+        </div>
+);
+
+
+
